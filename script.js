f1b8aa8fd6bdec4df5c457459a5a6b3429ac146f
mostone1 <129112059+mostone1@users.noreply.github.com>
2023-06-29 12:01:48 +0000

    f

---
+++ script.js
@@ -0,0 +1,25 @@
+const gameButton = document.getElementById('game-button');
+const scoreElement = document.getElementById('score');
+const audio = document.getElementById('audio');
+
+let score = 0;
+
+gameButton.addEventListener('click', increaseScore);
+
+function increaseScore() {
+  score++;
+  scoreElement.textContent = 'Очки: ' + score;
+  playSound('button-click.mp3');
+  animateButton();
+}
+
+function animateButton() {
+  gameButton.classList.remove('animate');
+  void gameButton.offsetWidth; // Перезавантаження анімації
+  gameButton.classList.add('animate');
+}
+
+function playSound(soundFile) {
+  audio.src = soundFile;
+  audio.play();
+}
